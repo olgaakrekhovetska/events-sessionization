19041996bed47505999dd9be89b1eea7b8a0bd68
Olga Krekhovetska <olgak@scribd.com>
2020-07-28 16:00:57 +0300

    v2: Used foldLeft to iterate through events

--- src/main/scala/com/scribd/Main.scala
+++ src/main/scala/com/scribd/Main.scala
@@ -51,38 +51,51 @@ object Main {
       Iterator.empty
     } else {
       logger.info(s"Got values for key=$key, state=${state.getOption}, currentWatermark: ${state.getCurrentWatermarkMs()}.")
-     // sort events by time in ascending order to update session
-      val events = values.toSeq.sortBy(_.time.getTime)
+      // sort events by time in ascending order to update session iteratively with bigger event time
+      val events = values.toSeq.sortBy(_.time.getTime).toList
       events.zipWithIndex.foreach { case (v, idx) => logger.info(s"$idx. $v") }
 
-      val sessions = events.map { event =>
-        val eventTimeMs: java.lang.Long = event.time.getTime
-        state.getOption match {
-          case Some(session) if (session.start <= eventTimeMs && eventTimeMs <= (session.end + periodOfInactivity)) =>
-            logger.info(s"Event is in scope of current session, session=$session, event=$event.")
-            val newSessionStart = if (session.start < eventTimeMs) session.start else eventTimeMs
-            val newSessionEnd = if (session.end < eventTimeMs) eventTimeMs else session.end
-            val updatedSession = session.copy(start = newSessionStart, end = newSessionEnd, eventsCount = session.eventsCount + 1)
-            state.update(updatedSession)
-            val expirationTime = updatedSession.end + periodOfInactivity
-            state.setTimeoutTimestamp(expirationTime)
-            logger.info(s"Updated state for key=${key}, state=${updatedSession}, expirationTime=$expirationTime, currentWatermark=${state.getCurrentWatermarkMs()}.")
-            updatedSession
-          case None =>
-            val newSession = SessionInfo(key, eventTimeMs, eventTimeMs, 1)
-            state.update(newSession)
-            val expirationTime = newSession.end + periodOfInactivity
-            state.setTimeoutTimestamp(expirationTime)
-            logger.info(s"Created new state for key=${key}, state=${newSession}, expirationTime=$expirationTime, currentWatermark=${state.getCurrentWatermarkMs()}.")
-            newSession
-          case Some(session) =>
-            logger.info(s"Event is out of scope for current session,  session=$session, event=$event.")
-            session
-        }
+      val updatedSession = state.getOption match {
+        case None =>
+          events match {
+            case head :: tail =>
+              val newSession = SessionInfo(key, head.time.getTime, head.time.getTime, 1)
+              logger.info(s"Created new state for key=${key}, state=${newSession}, currentWatermark=${state.getCurrentWatermarkMs()}.")
+              Some(updateSessionWithEvents(newSession, tail))
+            case _ => //if there are no events then we do not need to create session
+              None
+          }
+        case Some(existingSession) => Some(updateSessionWithEvents(existingSession, events))
       }
-      //taking last as it has the biggest end time as events were sorted in ASC order before
-      //TODO: use foldLeft
-      Iterator(sessions.last)
+
+      updatedSession match {
+        case Some(updatedSession) =>
+          state.update(updatedSession)
+          val expirationTime = updatedSession.end + periodOfInactivity
+          state.setTimeoutTimestamp(expirationTime)
+          logger.info(s"Updated state for key=${key}, state=${updatedSession}, expirationTime=$expirationTime, currentWatermark=${state.getCurrentWatermarkMs()}.")
+          Iterator(updatedSession)
+        case None => Iterator.empty
+      }
+    }
+  }
+
+  /** Update initial session state with events time, event should be sorted ih ASC order. */
+  private def updateSessionWithEvents(initialSessionState: SessionInfo, events: List[Event]): SessionInfo = {
+    events.foldLeft(initialSessionState) { case (session, event) =>
+      val eventTimeMs: java.lang.Long = event.time.getTime
+      eventTimeMs match {
+        case eventTime if (session.start <= eventTime && eventTime <= (session.end + periodOfInactivity)) =>
+          logger.info(s"Event=$event is in scope of current session=$session.")
+          val newSessionStart = if (session.start < eventTime) session.start else eventTime
+          val newSessionEnd = if (session.end < eventTime) eventTime else session.end
+          val updatedSession = session.copy(start = newSessionStart, end = newSessionEnd, eventsCount = session.eventsCount + 1)
+          updatedSession
+        case _ =>
+          logger.info(s"Event=$event is out of scope for current session=$session.")
+          //TODO this should be handeled in a way of creating new session and return list of sessions -- old session and new
+          session
+      }
     }
   }
 
